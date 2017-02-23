777053170148eb788c75f88c4a7d7254ae0d4d67
TrifonEstov <don.tito.g@gmail.com>
2017-02-23 23:37:28 +0200

    Program list updated!

---
+++ Lists/02.AppendList/02.AppendList.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.AppendList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split('|').ToArray();
+            var numbers = new List<int>();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                numbers.AddRange(input[i].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
