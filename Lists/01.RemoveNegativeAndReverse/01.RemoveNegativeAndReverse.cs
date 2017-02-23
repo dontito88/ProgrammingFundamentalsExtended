56ef401155683bedd9c0e00a1cfa70c567675154
TrifonEstov <don.tito.g@gmail.com>
2017-02-23 22:43:15 +0200

    new program added!

---
+++ Lists/01.RemoveNegativeAndReverse/01.RemoveNegativeAndReverse.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.RemoveNegativeAndReverse
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            inputList.Reverse();
+            var possitiveList = new List<int>();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (inputList[i] > 0)
+                {
+                    possitiveList.Add(inputList[i]);
+                }
+            }
+
+            if (possitiveList.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            foreach (var number in possitiveList)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
