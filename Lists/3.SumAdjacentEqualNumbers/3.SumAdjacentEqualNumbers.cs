8dec2a50ff908e9e6ed04b3b97e062ad701d5a98
TrifonEstov <don.tito.g@gmail.com>
2017-02-25 17:33:10 +0200

    Third problem does not work properly!

---
+++ Lists/3.SumAdjacentEqualNumbers/3.SumAdjacentEqualNumbers.cs
@@ -0,0 +1,27 @@
+﻿namespace _3.SumAdjacentEqualNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumAdjacentEqualNumbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    input[i] += input[i + 1];
+                    input.RemoveAt(i + 1);
+                }
+            }
+            foreach (var item in input)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
