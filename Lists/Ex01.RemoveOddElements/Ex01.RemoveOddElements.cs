bb7536d266b9319d9dc6e5b7fa5fe73cac32cd16
TrifonEstov <don.tito.g@gmail.com>
2017-02-26 17:30:02 +0200

    Update - exercise problems added!

---
+++ Lists/Ex01.RemoveOddElements/Ex01.RemoveOddElements.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex01.RemoveOddElements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RemoveOddElements
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var evenElements = new List<string>();
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    evenElements.Add(input[i]);
+                }
+            }
+            foreach (var item in evenElements)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
