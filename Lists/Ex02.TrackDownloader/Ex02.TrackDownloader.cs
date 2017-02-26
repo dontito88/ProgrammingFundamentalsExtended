bd1eed98a7e334347ccd7f3cd8abebab8e4825f0
TrifonEstov <don.tito.g@gmail.com>
2017-02-26 18:40:33 +0200

    Another problem solved!

---
+++ Lists/Ex02.TrackDownloader/Ex02.TrackDownloader.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex02.TrackDownloader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TrackDownloader
+    {
+        public static void Main()
+        {
+            var blackList = Console.ReadLine().Split();
+            var trackList = new List<string>();
+            var line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                var isInBlacklist = false;
+
+                foreach (var blacklistedWord in blackList)
+                {
+                    if (line.Contains(blacklistedWord))
+                    {
+                        isInBlacklist = true;
+                        break;
+                    }
+                }
+
+                if (!isInBlacklist)
+                {
+                    trackList.Add(line);
+                }
+
+                line = Console.ReadLine();
+
+            }
+
+            trackList.Sort();
+
+            foreach (var item in trackList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
