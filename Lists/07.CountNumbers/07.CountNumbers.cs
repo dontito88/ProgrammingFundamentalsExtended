12fe8a053c71ae782f551a35052b8f32255ab9f3
TrifonEstov <don.tito.g@gmail.com>
2017-02-26 16:42:09 +0200

    All problems solved! Some of them are not working properly!!!

---
+++ Lists/07.CountNumbers/07.CountNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace _07.CountNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountNumbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            input.Sort();
+            var count = new int[1001];
+
+            foreach (var item in input)
+            {
+                count[item]++;  
+            }
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > 0)
+                {
+                    Console.WriteLine($"{i} -> {count[i]}");
+                }
+            }
+        }
+    }
+}
