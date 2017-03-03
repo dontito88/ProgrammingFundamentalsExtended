5d5c2b52637da8f5709dbb21530742a0eadc7b00
TrifonEstov <don.tito.g@gmail.com>
2017-03-03 18:10:42 +0200

    New program added!

---
+++ Lists/Ex03.EqualSum/Ex03.EqualSum.cs
@@ -0,0 +1,39 @@
+﻿namespace Ex03.EqualSum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class EqualSum
+    {
+        public static void Main()
+        {
+            var firstListOfInt = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var secondListOfInt = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            foreach (var item in firstListOfInt)
+            {
+                secondListOfInt.Remove(item);
+            }
+
+            var sumFirts = firstListOfInt.Sum();
+            var sumSecond = secondListOfInt.Sum();
+            var diff = Math.Abs(sumFirts - sumSecond);
+            if (sumFirts == sumSecond)
+            {
+                Console.WriteLine($"Yes. Sum: {sumFirts}");
+            }
+            else
+            {
+                Console.WriteLine($"No. Diff: {diff}");
+            }
+        }
+    }
+}
