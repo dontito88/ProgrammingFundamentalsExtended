9871e20cd885ec9ab41fc99d7aa8606320be3761
TrifonEstov <don.tito.g@gmail.com>
2017-03-05 17:53:58 +0200

    New solution added!

---
+++ ArrayAndListAlgorithms/ArrayAndListAlgorithms/1ArrayContainsElement/ArrayContainsElement.cs
@@ -0,0 +1,26 @@
+﻿namespace _1ArrayContainsElement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ArrayContainsElement
+    {
+        public static void Main()
+        {
+            var inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int containedNum = int.Parse(Console.ReadLine());
+
+            bool isContain = inputArray.Contains(containedNum);
+
+            if (isContain)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
