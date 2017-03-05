cfcff101c930c9b0c4dec730c8b0e887fba32218
TrifonEstov <don.tito.g@gmail.com>
2017-03-05 18:06:19 +0200

    new program added!

---
+++ ArrayAndListAlgorithms/ArrayAndListAlgorithms/2SmallestElement/SmallestElement.cs
@@ -0,0 +1,18 @@
+﻿namespace _2SmallestElement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SmallestElement
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            input.Sort();
+
+            Console.WriteLine(input[0]);
+        }
+    }
+}
