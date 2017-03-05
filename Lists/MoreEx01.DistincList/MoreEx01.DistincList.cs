2a6a61a45caabfb2267b670c117f481a02c522cb
TrifonEstov <don.tito.g@gmail.com>
2017-03-05 17:24:59 +0200

    DistincList doesn't work properly!

---
+++ Lists/MoreEx01.DistincList/MoreEx01.DistincList.cs
@@ -0,0 +1,31 @@
+﻿namespace MoreEx01.DistincList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < inputList.Count - 1; i++)
+            {
+                var firstElement = inputList[0];
+                if (inputList.Contains(firstElement))
+                {
+                    inputList.Remove(firstElement);
+                }
+                else
+                {
+                    Console.WriteLine("No repeating elements");
+                }
+            }
+            foreach (var item in inputList)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
