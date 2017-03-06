687d57bd3e8c3d06ed28aa2059ae048b238f9784
TrifonEstov <don.tito.g@gmail.com>
2017-03-06 22:36:20 +0200

    Last problem from labs solved!

---
+++ ArrayAndListAlgorithms/ArrayAndListAlgorithms/7LargestNelements/LargestNelements.cs
@@ -0,0 +1,25 @@
+﻿namespace _7LargestNelements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LargestNelements
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            var n = int.Parse(Console.ReadLine());
+
+            input.Sort();
+
+            for (int i = input.Count - 1; i > input.Count - n - 1; i--)
+            {
+                Console.Write(input[i] + " ");
+            }
+        }
+    }
+}
