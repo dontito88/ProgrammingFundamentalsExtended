6a172c48c6f892fb72b05545f7188140f1a34926
TrifonEstov <don.tito.g@gmail.com>
2017-03-06 20:04:59 +0200

    New problem solved!

---
+++ ArrayAndListAlgorithms/ArrayAndListAlgorithms/3ReverseArray/ReverseArray.cs
@@ -0,0 +1,24 @@
+﻿namespace _3ReverseArray
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ReverseArray
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            input.Reverse();
+
+            foreach (var item in input)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
