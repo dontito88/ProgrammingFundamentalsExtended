69519f21a8be1e3ac9a892da0b4927ddbe827de5
TrifonEstov <don.tito.g@gmail.com>
2017-03-06 22:21:48 +0200

    SortedArrays work properly but not made by the exercise condition!

---
+++ ArrayAndListAlgorithms/ArrayAndListAlgorithms/4SortedArrays/SortedArrays.cs
@@ -0,0 +1,19 @@
+﻿namespace _4SortedArrays
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SortedArrays
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            input.Sort();
+            foreach (var item in input)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
