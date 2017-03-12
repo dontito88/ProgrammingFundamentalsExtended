f589d0fbe17aa254720d6c7e28d6934e44587712
TrifonEstov <don.tito.g@gmail.com>
2017-03-12 17:01:09 +0200

    First problem solved!

--- Dictionaries/01.CountRealNumbers/CountRealNumbers.cs
+++ Dictionaries/01.CountRealNumbers/CountRealNumbers.cs
@@ -8,8 +8,25 @@
     {
         public static void Main()
         {
-            var input = Console.ReadLine().Split(' ').Select(double.Parse);
-            input = new SortedDictionary<double, double>();
+            var numbers = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            var result = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result[number] = 0;
+                }
+
+                result[number]++;
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
         }
     }
 }
