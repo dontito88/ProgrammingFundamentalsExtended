bd9a23958b50cf70de930be3832dde6212f3ddeb
TrifonEstov <don.tito.g@gmail.com>
2017-03-12 16:32:37 +0200

    New project added!

---
+++ Dictionaries/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(double.Parse);
+            input = new SortedDictionary<double, double>();
+        }
+    }
+}
