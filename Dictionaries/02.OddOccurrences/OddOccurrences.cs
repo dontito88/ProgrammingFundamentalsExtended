e5962483d492edffde30396df4145e7c89dc012d
TrifonEstov <don.tito.g@gmail.com>
2017-03-17 20:09:11 +0200

    update

---
+++ Dictionaries/02.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.OddOccurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class OddOccurrences
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!result.ContainsKey(word))
+                {      
+                    result[word] = 0;
+                }
+
+                result[word]++;
+            }
+
+            var oddNumbesWords = new List<string>();
+
+            foreach (var kvp in result)
+            {
+                var value = kvp.Value;
+                if (value % 2 != 0)
+                {
+                    oddNumbesWords.Add(kvp.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", oddNumbesWords));
+        }
+    }
+}
