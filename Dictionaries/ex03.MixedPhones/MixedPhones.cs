81c8f690c4ad0a24aee7eeca2cd3e8568e6fbf48
TrifonEstov <don.tito.g@gmail.com>
2017-03-18 11:52:14 +0200

    update

---
+++ Dictionaries/ex03.MixedPhones/MixedPhones.cs
@@ -0,0 +1,43 @@
+﻿namespace ex03.MixedPhones
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MixedPhones
+    {
+        public static void Main()
+        {
+            var phoneBook = new SortedDictionary<string, decimal>();
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "Over")
+            {
+                var tokens = inputLine
+                    .Split(" :".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                var firstElement = tokens[0];
+                var secondElement = tokens[1];
+
+                var phoneNumber = 0m;
+                if (decimal.TryParse(firstElement, out phoneNumber))
+                {
+                    phoneBook[secondElement] = phoneNumber;
+                }
+                else if (decimal.TryParse(secondElement, out phoneNumber))
+                {
+                    phoneBook[firstElement] = phoneNumber;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var item in phoneBook)
+            {
+                var name = item.Key;
+                var number = item.Value;
+                Console.WriteLine($"{name} -> {number}");
+            }
+        }
+    }
+}
