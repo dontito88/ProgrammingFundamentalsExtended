f07e2d69c38ece08db1679119b8f73349ea588a8
TrifonEstov <don.tito.g@gmail.com>
2017-03-21 22:33:39 +0200

    update

---
+++ AdvancedCollections/03.RecordUniqueNames/RecordUniqueNames.cs
@@ -0,0 +1,26 @@
+﻿namespace _03.RecordUniqueNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RecordUniqueNames
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var nameSet = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputLine = Console.ReadLine();
+                nameSet.Add(inputLine);
+            }
+
+            foreach (var item in nameSet)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
