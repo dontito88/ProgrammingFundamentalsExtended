83d23fc1e198464046984d88e3de5d179f20d610
TrifonEstov <don.tito.g@gmail.com>
2017-03-22 00:23:42 +0200

    update

---
+++ AdvancedCollections/ex01.Shellbound/Shellbound.cs
@@ -0,0 +1,39 @@
+﻿namespace ex01.Shellbound
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Shellbound
+    {
+        public static void Main()
+        {
+            var vladiSshell = new Dictionary<string, HashSet<int>>();
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "Aggregate")
+            {
+                var shellInfo = inputLine.Split(' ');
+                var region = shellInfo[0];
+                var size = int.Parse(shellInfo[1]);
+
+                if (!vladiSshell.ContainsKey(region))
+                {
+                    vladiSshell[region] = new HashSet<int>();
+                }
+                vladiSshell[region].Add(size);
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var item in vladiSshell)
+            {
+                var regionName = item.Key;
+                var shells = item.Value;
+                var sumOfShells = shells.Sum();
+                var giantShell = sumOfShells - sumOfShells / shells.Count;
+                Console.WriteLine($"{regionName} -> {string.Join(", ", shells)} ({giantShell})");
+            }
+        }
+    }
+}
