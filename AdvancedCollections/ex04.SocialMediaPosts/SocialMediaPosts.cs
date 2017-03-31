a0c82f4a6b92a2e4bad6e76e98ffea8315e1b498
TrifonEstov <don.tito.g@gmail.com>
2017-03-31 22:04:37 +0300

    update

---
+++ AdvancedCollections/ex04.SocialMediaPosts/SocialMediaPosts.cs
@@ -0,0 +1,30 @@
+﻿namespace ex04.SocialMediaPosts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SocialMediaPosts
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var like = new Dictionary<string, int>();
+            var dislike = new Dictionary<string, int>();
+            var comment = new Dictionary<string, Dictionary<string, string>>();
+
+            while (inputLine != "drop the medi")
+            {
+
+                string firstElement = inputLine[0];
+                
+                if (firstElement == "post")
+                {
+
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
