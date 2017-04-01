1af4b9d03f15308ff96ecb97766c552175f05246
TrifonEstov <don.tito.g@gmail.com>
2017-04-01 21:43:25 +0300

    update

---
+++ LambdaAndLINQ/LambdaAndLINQ/02.Largerst3Numbers/Largerst3Numbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.Largerst3Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Largerst3Numbers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList<int>()
+                .OrderByDescending(x => x)
+                .Take(3);
+
+            foreach (var item in input)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
