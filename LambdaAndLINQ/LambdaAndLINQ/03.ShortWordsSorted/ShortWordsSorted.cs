b9c001eff2852eb532938626afaddd00fe69556f
TrifonEstov <don.tito.g@gmail.com>
2017-04-02 01:19:38 +0300

    update

---
+++ LambdaAndLINQ/LambdaAndLINQ/03.ShortWordsSorted/ShortWordsSorted.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.ShortWordsSorted
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ShortWordsSorted
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .ToLower()
+                .Split(new[] { ' ', '.', ',', '/', '\\', ':', ';', ')', '(', '!', '?' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= 4)
+                .OrderBy(x => x)
+                .Distinct()
+                .ToList<string>();
+            
+                Console.Write(string.Join(", ", input));
+        }
+    }
+}
