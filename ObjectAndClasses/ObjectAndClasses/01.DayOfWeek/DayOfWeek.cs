bc60628d354a76fe843eb7fde807ff836926ac4f
TrifonEstov <don.tito.g@gmail.com>
2017-04-04 23:22:45 +0300

    update

---
+++ ObjectAndClasses/ObjectAndClasses/01.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.DayOfWeek
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DayOfWeek
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split('-').ToList();
+            int day = int.Parse(input[0]);
+            int month = int.Parse(input[1]);
+            int year = int.Parse(input[2]);
+
+            var date = new DateTime(year, month, day);
+            Console.WriteLine(date.DayOfWeek);
+
+        }
+    }
+}
