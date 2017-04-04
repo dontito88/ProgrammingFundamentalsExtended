120c0cacfdffd2bcdcce1fd082a6e7ace2f722df
TrifonEstov <don.tito.g@gmail.com>
2017-04-04 23:41:19 +0300

    update

---
+++ ObjectAndClasses/ObjectAndClasses/02.RandomizeWords/RandomizeWords.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.RandomizeWords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RandomizeWords
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').ToArray();
+
+            var random = new Random();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentWord = input[i];
+                var randomPossition = random.Next(0, input.Length);
+                var temporaryWord = input[randomPossition];
+                input[randomPossition] = currentWord;
+                input[i] = temporaryWord;
+            }
+
+            foreach (var word in input)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
