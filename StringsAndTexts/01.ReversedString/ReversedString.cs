94d94cc087c3ab21e38c23213acd6aacca1d0bfa
TrifonEstov <don.tito.g@gmail.com>
2017-04-19 00:14:49 +0300

    new Solution uploaded!

---
+++ StringsAndTexts/01.ReversedString/ReversedString.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.ReversedString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class ReversedString
+    {
+        static void Main()
+        {
+            var text = Console.ReadLine()
+                .ToArray()
+                .Reverse();
+
+            foreach (var item in text)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
