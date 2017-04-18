e8310fab4737404484df92ec52d04f468c7f51b5
TrifonEstov <don.tito.g@gmail.com>
2017-04-19 00:35:15 +0300

    update

---
+++ StringsAndTexts/03.TextFilter/TextFilter.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.TextFilter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TextFilter
+    {
+        public static void Main()
+        {
+            var bannedWors = Console.ReadLine()
+                .Split(new[] { ' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+
+            var text = Console.ReadLine();
+
+            foreach (var word in bannedWors)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
