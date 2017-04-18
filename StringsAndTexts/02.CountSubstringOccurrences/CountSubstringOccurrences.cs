af566176a0ab06ee57d32746183acad5d4c859b0
TrifonEstov <don.tito.g@gmail.com>
2017-04-19 00:30:06 +0300

    update

---
+++ StringsAndTexts/02.CountSubstringOccurrences/CountSubstringOccurrences.cs
@@ -0,0 +1,31 @@
+﻿namespace _02.CountSubstringOccurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountSubstringOccurrences
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var pattern = Console.ReadLine();
+
+            var count = -0;
+            var index = -1;
+
+            while (true)
+            {
+                index = text.IndexOf(pattern, index + 1);
+                if (index < 0)
+                {
+                    break;
+                }
+
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }   
+}
