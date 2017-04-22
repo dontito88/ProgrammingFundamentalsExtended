7a8b64c505b58b6226923ea08151e47a7d142aa6
TrifonEstov <don.tito.g@gmail.com>
2017-04-22 17:58:44 +0300

    update - regex.replacetag does not work properly

---
+++ StringsAndTexts/RegEx.ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,21 @@
+﻿namespace RegEx.ReplaceTag
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class ReplaceTag
+    {
+        public static void Main()
+        {
+            var pattern = @"<a.+?href=(.+?)>(.+?)<\/a>";
+            var regex = new Regex(pattern);
+
+            var text = Console.ReadLine();
+            var replacement = "[URL href=$1]$2[/URL]";
+            var result = regex.Replace(text, replacement);
+            Console.WriteLine(result);
+        }
+    }
+}
