08f53d741216d84302be6c3890756c3f4ea84ef7
TrifonEstov <don.tito.g@gmail.com>
2017-04-23 14:11:39 +0300

    Final uploads - exam time is coming!

---
+++ ExamPreparation/Test1.01.SinoTheWalker/SinoTheWalker.cs
@@ -0,0 +1,27 @@
+﻿namespace Test1._01.SinoTheWalker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    public class SinoTheWalker
+    {
+        public static void Main()
+        {
+            var timeFormat = @"hh\:mm\:ss";
+            var leavingTime = TimeSpan.ParseExact(Console.ReadLine(), timeFormat, CultureInfo.InvariantCulture);
+
+            var stepsNeeded = int.Parse(Console.ReadLine());
+            var secondsPerStep = int.Parse(Console.ReadLine());
+
+            var secondsPerDay = 60 * 60 * 24;
+            var totalSecondsNeeded = (int)((double)stepsNeeded * secondsPerStep % secondsPerDay);
+
+            var arrivalTime = leavingTime.Add(new TimeSpan(0, 0, totalSecondsNeeded));
+
+            Console.WriteLine($"Time Arrival: {arrivalTime.ToString(timeFormat)}");
+
+        }
+    }
+}
