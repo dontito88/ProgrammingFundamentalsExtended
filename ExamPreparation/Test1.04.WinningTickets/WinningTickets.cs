1b8a0864b76505a68c80320b9d414db4d729a5ea
TrifonEstov <don.tito.g@gmail.com>
2017-04-25 23:11:51 +0300

    update

--- ExamPreparation/Test1.04.WinningTickets/WinningTickets.cs
+++ ExamPreparation/Test1.04.WinningTickets/WinningTickets.cs
@@ -3,12 +3,45 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text.RegularExpressions;
 
     public class Program
     {
         public static void Main()
         {
-            
+            const string pattern = @"([$#^^@])\1{5,}";
+            var tickets = Console.ReadLine().Split(',').Select(a => a.Trim()).ToArray();
+            var ticketRegEx = new Regex(pattern);
+
+            foreach (var ticket in tickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                var leftHalf = ticket.Substring(0, ticket.Length / 2);
+                var rightHalf = ticket.Substring(ticket.Length / 2);
+
+                var leftMatch = ticketRegEx.Match(leftHalf);
+                var rightMatch = ticketRegEx.Match(rightHalf);
+
+                if (leftMatch.Success && rightMatch.Success)
+                {
+                    var winningSymbol = leftMatch.Value[0];
+                    var shorterMatch = Math.Min(leftMatch.Length, rightMatch.Length);
+
+                    var jackpot = string.Empty;
+
+                    if (shorterMatch == 10)
+                    {
+                        jackpot = "jackpot";
+                    }
+
+                    Console.WriteLine($"ticket \"{ticket}\" - {shorterMatch}{winningSymbol}{jackpot}");
+                }
+            }
         }
     }
 }
