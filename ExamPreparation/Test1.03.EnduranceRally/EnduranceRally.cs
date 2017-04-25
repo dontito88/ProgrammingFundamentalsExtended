11482ba848071dda40089ee984568673b844fb33
TrifonEstov <don.tito.g@gmail.com>
2017-04-25 21:19:10 +0300

    update

---
+++ ExamPreparation/Test1.03.EnduranceRally/EnduranceRally.cs
@@ -0,0 +1,59 @@
+﻿namespace Test1._03.EnduranceRally
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class EnduranceRally
+    {
+        public static void Main()
+        {
+            var participants = Console.ReadLine().Split();
+
+            var trackIndices = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            var checkpointIndices = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            foreach (var participant in participants)
+            {
+                double fuel = participant[0];
+                
+                for (int currentTrackIndex = 0; currentTrackIndex < trackIndices.Length; currentTrackIndex++)
+                {
+                    bool outOfFuel = fuel <= 0;
+                    bool reachedEndOfTrack = currentTrackIndex == trackIndices.Length;
+                    var raceEnded = outOfFuel || reachedEndOfTrack;
+
+                    if (raceEnded)
+                    {
+
+                        if (outOfFuel)
+                        {
+                            Console.WriteLine($"{participant} - reached {currentTrackIndex - 1}");
+                        }
+                        else if (reachedEndOfTrack)
+                        {
+                            Console.WriteLine($"{participant} - fuel left {fuel:F2}");
+                        }
+
+                        break;
+                    }
+
+                    var fuelAtCheckpoint = trackIndices[currentTrackIndex];
+
+                    var isCheckpoint = checkpointIndices.Contains(currentTrackIndex);
+
+                    if (isCheckpoint)
+                    {
+                        fuel += fuelAtCheckpoint;
+                    }
+                    else
+                    {
+                        fuel -= fuelAtCheckpoint;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation/Test1.04.WinningTickets/WinningTickets.cs
@@ -0,0 +1,14 @@
+﻿namespace Test1._04.WinningTickets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
