7e2c6f5e22a3d7ab9ea91763b63f686cdf2ba273
TrifonEstov <don.tito.g@gmail.com>
2017-04-27 00:30:29 +0300

    update

---
+++ ExamPreparation/Test2.01.ChariryMarathon/CharityMarathon.cs
@@ -0,0 +1,19 @@
+﻿namespace Test2._01.ChariryMarathon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            var marathonDays = int.Parse(Console.ReadLine());
+            var runners = int.Parse(Console.ReadLine());
+            var averageLapsOnTrack = int.Parse(Console.ReadLine());
+            var lapLenght = int.Parse(Console.ReadLine());
+            var trackCapacity = int.Parse(Console.ReadLine());
+            var moneyPerKilometer = double.Parse(Console.ReadLine());
+        }
+    }
+}
